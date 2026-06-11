24a42fabd83e875ed9943fc743dbc58cfb01a46c
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2026-06-11 14:26:21 +0800

    feat: add routing functionality with router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Home from "./module/homepage";
-import Nav from "./module/nav";
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from './module/homepage';
+import Nav from './module/nav';
 
 function App() {
-
-  return (
-    <>
-      <Nav />
-      <Home />
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Nav />
+			<Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/' element={<></>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
